5388b274eb3be334c800b0d9460e458e36651df4
Randy Taylor <randyisbusy@gmail.com>
2019-08-17 20:57:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 class Student < User
-  attr_accessor :name
-  def initialize(name)
-    @name = name
+  attr_accessor :first_name, :last_name
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
   end
 end
